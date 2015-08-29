1d0bd67bbb296defcbdf779f38893fc4c8c6801b
Hunter MacDermut <hunter.macdermut@coxinc.com>
2015-08-29 14:37:33 -0400

    Wire-up basic user auth using Passport

--- server.js
+++ server.js
@@ -2,12 +2,6 @@
 var express = require('express');
 var app = express();
 
-// setup Swig for templating
-var swig = require('swig');
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
-app.set('views', __dirname + '/templates');
-
 // import Mongo, setup db, connect to db
 var db;
 var MongoClient = require('mongodb').MongoClient;
@@ -22,8 +16,55 @@ MongoClient.connect('mongodb://localhost:27017/blog', function(err, database){
     });
 });
 
+// setup Passport for user auth
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+    function(username, password, done){
+        db.collection('users').findOne({username: username}, function(err, user){
+            if (user === null){
+                return done(null, false);
+            }
+            if (err) {return done(err);}
+            if (username !== user.username){
+                return done(null, false);
+            }
+            if (password !== user.password){
+                return done(null, false);
+            }
+            return done(null, user);
+        });
+    }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.username);
+});
+
+passport.deserializeUser(function(user, done) {
+    // find user, add user object to request
+    db.collection('users').findOne({username: user}, function(err, user){
+        return done(null, user);
+    });
+});
+
+// setup Swig for templating
+var swig = require('swig');
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/templates');
+
 // look in 'public' folder for css and js files
 app.use(express.static('public'));
+
+// setup body-parser for adding form data to the response
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+var session = require('express-session');
+app.use(session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // routes
 app.get('/', function(request, response){
@@ -63,6 +104,25 @@ app.get('/tag/:tag', function(request, response){
             response.render('tag', {posts: docs, tag: request.params.tag});
         }
     });
+});
+
+app.get('/login', function(request, response){
+    response.render('login');
+});
+
+app.post('/login',
+    passport.authenticate('local', {
+        successRedirect: '/add',
+        failureRedirect: '/login'
+    })
+);
+
+app.get('/add', function(request, response){
+    if (request.isAuthenticated()){
+        response.render('add');
+    } else {
+        response.redirect('/login');
+    }
 });
 
 // custom middleware for error handling (run this if all other routes fail)
